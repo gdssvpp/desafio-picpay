6aa53f49c067dd96b2c8afdda65ef28ad99de3a5
gdssvpp <pablonogueira16@hotmail.com>
2023-09-07 18:58:50 -0300

    feat:
    create transactional package and class

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transactional/Transaction.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.domain.transactional;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.cglib.core.Local;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timeStamp;
+}
