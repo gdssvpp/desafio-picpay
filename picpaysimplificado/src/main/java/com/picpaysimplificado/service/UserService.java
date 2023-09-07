4336780034ff7b914f3cbd374ae03e9d34fef2d4
gdssvpp <pablonogueira16@hotmail.com>
2023-09-07 20:13:44 -0300

    feat: create userService and some methods

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/service/UserService.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.service;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionException;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuário do tipo LOJISTA não está autoizado a realizar transação");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo Insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
